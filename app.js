bbb52691906284be69aeeffbed3a5acd02a84c5d
Mesfer <mesfer.vip@gmail.com>
2020-10-19 11:09:05 +0300

    create two methods saveInput and confirmInput

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const app = Vue.createApp({
+
+    data(){
+        return{
+            userInput:'',
+            confirmedInput:''
+        };
+    },
+
 methods:{
     showAlert(){
         alert('IT IS WORKING .....HYE!!!!!');
+    },
+    saveInput(e){
+        this.userInput = e.target.value;
+    },
+    confirmInput(){
+        this.confirmedInput = this.userInput;
     }
 }
 });
